c19246545078726c68548fb4805f0cce1305f17e
Trần Quang Vũ <sacker961@gmail.com>
2018-11-15 11:44:29 +0700

    fix lai refactorin

--- AreaSearchController.php
+++ AreaSearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Http\Requests\AreaDisplayRequest;
 use App\Http\Requests\AreaSearchRequest;
 use App\Models\Area2Search;
 use Illuminate\Http\Request;
@@ -15,98 +16,69 @@ use App\Models\City;
 use App\Models\Area1Display;
 
 
-/**
- * Class BarsController
- *
- * @category  Controller
- * @package   App\Http\Controllers
- * @author    Tran Quang Vu
- * @copyright 2018 kyujin
- * @license   https://opensource.org/licenses/mit-license.php MIT License
- * @link      https://laravel.com Laravel(tm) Project
- */
-
-class AreaSearchController extends ParentController
+class AreaDisplayController extends ParentController
 {
     public function index(Request $request)
     {
-        if ($request->session()->exists(SESSION_AREA_SEARCH_SELECTED)) {
-            $cityID = $request->session()->get(SESSION_AREA_SEARCH_SELECTED);
-            $areas = Area2Search::getAllByCityIdPluck($cityID);
-        } else {
+        if($request->session()->exists(SESSION_AREA_DISPLAY_SELECTED)) {
+            $cityID = $request->session()->get(SESSION_AREA_DISPLAY_SELECTED);
+            $areas = Area1Display::getAllByCityIdPluck($cityID);
+        }else {
             $areas = [];
         }
         $cities = City::all();
-        return view('area_search.index', compact('cities', 'areas'));
+        return view('area_display.index', compact('cities', 'areas'));
     }
 
-    public function search(Request $request)
+    public function display(Request $request)
     {
         $cityID = $request->get('city_id');
-        $request->session()->put(SESSION_AREA_SEARCH_SELECTED, $cityID);
+        $request->session()->put(SESSION_AREA_DISPLAY_SELECTED, $cityID);
     }
 
-    /**
-     * @param  $area_id
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
     public function delete($areaID)
     {
-        $area = Area2Search::class;
+        $area = Area1Display::class;
         $user = Auth::guard('admin')->user();
         if ($user->cannot('deleteArea', $area)) {
-            return redirect()->route(ADMIN_AREA_SEARCH);
+            return redirect()->route(ADMIN_AREA_DISPLAY);
         }
-        if (!Area2Search::Area2SearchHavePost($areaID)) {
-            $areaSearch = Area2Search::find($areaID);
-            if($areaSearch) {
-                $areaSearch->delete();
+        if (!Area1Display::Area1DisplayHavePost($areaID)) {
+            $areaDisplay = Area1Display::find($areaID);
+            if($areaDisplay){
+                $areaDisplay->delete();
                 Session::flash('alert-success', __('messages.area.delete_success'));
             }
         } else {
             Session::flash('alert-error', __('messages.area.delete_error'));
         }
-        return redirect(route(ADMIN_AREA_SEARCH));
+        return redirect(route(ADMIN_AREA_DISPLAY));
     }
 
-    /**
-     * @param  Request $request
-     * @param  $area_id
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
     public function edit(Request $request, $areaID)
     {
-
-        $area = Area2Search::find($areaID);
+        $area = Area1Display::find($areaID);
         $area->fill($request->all());
         if ($area->save()) {
             Session::flash("alert-success", __("messages.area.edit_success"));
         } else {
             Session::flash("alert-error", __("messages.area.edit_error"));
         }
-        return redirect(route(ADMIN_AREA_SEARCH));
+        return redirect(route(ADMIN_AREA_DISPLAY));
     }
 
-    /**
-     * Check Edit
-     *
-     * @param Request $request request
-     *
-     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
-     */
     public function checkEdit(Request $request)
     {
         $areaName = $request->get('area_name');
         $areaId = $request->get('area_id');
         $areaName = preg_replace('/\s+/', ' ', $areaName);
-        $maxlen = 100;
         if ($this->checkDuplicateArea($areaName, $areaId)) {
             return response('duplicate', 200);
         }
         if ($areaName == '') {
             return response('error', 200);
         }
-        if (strlen($areaName) > $maxlen) {
+        if (strlen($areaName) > 100) {
             return response('fail', 200);
         }
         return response('success', 200);
@@ -114,8 +86,8 @@ class AreaSearchController extends ParentController
 
     public function checkDuplicateArea($name, $id)
     {
-        $cityID = session()->get(SESSION_AREA_SEARCH_SELECTED);
-        $areas = Area2Search::getAllByCityIdPluck($cityID);
+        $cityID = session()->get(SESSION_AREA_DISPLAY_SELECTED);
+        $areas = Area1Display::getAllByCityIdPluck($cityID);
         if (array_search($name,$areas)) {
             if (array_search($name,$areas) != $id) {
                 return true;
@@ -126,65 +98,57 @@ class AreaSearchController extends ParentController
 
     public function addUp(Request $request)
     {
-        $cityID = $request->session()->get(SESSION_AREA_SEARCH_SELECTED);
-        $areas = Area2Search::getAllByCityIdPluck($cityID);
-        $area = new Area2Search();
+        $cityID = $request->session()->get(SESSION_AREA_DISPLAY_SELECTED);
+        $areas = Area1Display::getAllByCityIdPluck($cityID);
+        $area = new Area1Display();
         $area->fill($request->all());
         $area->name = preg_replace('/\s+/', ' ', $area->name);
         $area->order = 0;
-        checkAddUp($area->name, $area, $areas, $area->id, $cityID);
-        return redirect(route(ADMIN_AREA_SEARCH));
-    }
-
-    public function checkAddUp($name, $area, $areas, $id, $cityID){
-        $maxlen = 100;
-        if ($name === '') {
+        if($area->name == '') {
             Session::flash("alert-error", __("messages.area.add_required_name"));
         }
-        elseif (strlen($name) > $maxlen) {
+        elseif(strlen($area->name) > 100) {
             Session::flash("alert-error", __("messages.area.add_max_100_name"));
         }
-        elseif (array_search($name, $areas) != false) {
+        elseif(array_search($area->name, $areas) != false) {
             Session::flash("alert-error", __("messages.area.add_unique_name"));
         }
         elseif ($area->save()) {
-            Area2Search::updateOrderByAreaAddUp($id, $cityID);
+            Area1Display::updateOrderByAreaAddUp($area->id, $cityID);
             Session::flash("alert-success", __("messages.area.addup_success"));
         }
+        return redirect(route(ADMIN_AREA_DISPLAY));
     }
 
     public function addDown(Request $request)
     {
-        $cityID = $request->session()->get(SESSION_AREA_SEARCH_SELECTED);
-        $areas = Area2Search::getAllByCityIdPluck($cityID);
-        $area = new Area2Search();
+        $cityID = $request->session()->get(SESSION_AREA_DISPLAY_SELECTED);
+        $areas = Area1Display::getAllByCityIdPluck($cityID);
+        $area = new Area1Display();
         $area->fill($request->all());
         $area->name = preg_replace('/\s+/', ' ', $area->name);
-        checkAddDown($area->name, $area, $areas, $area->id, $cityID);
-        return redirect(route(ADMIN_AREA_SEARCH));
-    }
-
-     public function checkAddDown($name, $area, $areas, $id, $cityID){
-        $maxlen = 100;
-        if ($name === '') {
+        if($area->name == '') {
             Session::flash("alert-error", __("messages.area.add_required_name"));
         }
-        elseif(strlen($name) > $maxlen) {
+        elseif(strlen($area->name) > 100) {
             Session::flash("alert-error", __("messages.area.add_max_100_name"));
         }
-        elseif(array_search($name, $areas) != false) {
+        elseif(array_search($area->name, $areas) != false) {
             Session::flash("alert-error", __("messages.area.add_unique_name"));
         }
         elseif ($area->save()) {
-            Area2Search::updateOrderByAreaAddDown($cityID, $id);
+            Area1Display::updateOrderByAreaAddDown($cityID, $area->id);
             Session::flash("alert-success", __("messages.area.addup_success"));
         }
+        return redirect(route(ADMIN_AREA_DISPLAY));
     }
+
+
 
     public function order(Request $request)
     {
         $params = $request->all();
-        if (Area2Search::orderArea2Search($params['ids'])) {
+        if (Area1Display::orderArea1Display($params['ids'])) {
             return response()->json(true);
         }
         return response()->json(false);
